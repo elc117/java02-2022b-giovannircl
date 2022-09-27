c8487111af3056a3803e08c3dda06cdb1714949c
giovannircl <60160232+giovannircl@users.noreply.github.com>
2022-09-26 22:41:09 -0300

    Ajeitando a indentação

--- display.java
+++ display.java
@@ -1,30 +1,30 @@
 public class display extends Main {
-    private double inches; // polegadas do display
+	private double inches; // polegadas do display
 	private boolean led; // é de led ou não
 	private int brightness; // nits
 	private boolean converted;
 	    
 	//Construtor
 	public display(double i, boolean l, int b) {
-	    this.setInches(i);
-	    this.setLed(l);
+		this.setInches(i);
+		this.setLed(l);
 	    this.setBrightness(b);
 	    this.converter();
 	} 
 	    
 	//Setters
 	public void setInches(double i) { this.inches = i; }
-    public void setLed(boolean l) { this.led = l; }
+	public void setLed(boolean l) { this.led = l; }
     public void setBrightness(int b) { this.brightness = b; }
         
     //Getters
-    public double getInches() { return this.inches; }
+	public double getInches() { return this.inches; }
     public boolean getLed() { return this.led; }
     public int getBrightness() { return this.brightness; }
         
     //Exibe o status do display utilizando os getters
     public void status() {
-        this.text(this.converted);
+    	this.text(this.converted);
         System.out.println("Led: " + this.getLed());
         System.out.println("Brightness: " + this.getBrightness());
         System.out.println("\n----------------------\n");
